d6d9aee3dc72d545cb5cd0519fdac184c3286dc8
renzhongyu <renzy3514@163.com>
2017-03-31 16:53:41 +0800

    learn lei

---
+++ index_words.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: UTF-8 -*-
+def index_words(text):
+    result = []
+    if text:
+        result.append(0)
+    for index, letter in enumerate(text, 1):
+        if letter == ' ':
+            result.append(index)
+    return result
+
+text = """The Zen of Python, by Tim Peters"""
+print(index_words(text))
